b92a393f1adfc669edf509d60e10fefb6e3d4732
salferdez <151395807+salferdez@users.noreply.github.com>
2024-08-21 18:05:43 +0200

    Create Combinatoric.hs

---
+++ Combinatoric.hs
@@ -0,0 +1,60 @@
+module Combinatoric
+    (
+      factorial
+    , combination
+    , permutation
+    , combinationR
+    , length'
+    , sum'
+    , permutationRepKnown
+    , stirling
+
+    ) where
+
+factorial :: Integer -> Integer
+factorial n
+    | n < 0     = error "input must be non negative"
+    | n == 0    = 1
+    | otherwise = accum n 1
+        where
+            accum 1 a = a
+            accum n a = accum (n-1) (a*n)
+
+combination :: Integer -> Integer -> Integer
+combination n k = factorial n `div` (factorial k * factorial (n-k))
+
+permutation :: Integer -> Integer -> Integer
+permutation n k = factorial n `div` factorial (n-k)
+
+combinationR :: Integer -> Integer -> Integer
+combinationR n k = combination (n+k-1) k
+
+length' :: [a] -> Integer
+length' x = helper 0 x
+    where
+        helper n []     = n
+        helper n (x:xs) = helper (n+1) xs
+
+sum' :: [Integer] -> Integer
+sum' x = accum 0 x
+    where
+        accum n []     = n
+        accum n (x:xs) = accum (n+x) xs
+
+permutationRepKnown :: Integer -> [Integer] -> Integer
+permutationRepKnown n (x:xs)
+    | length' (x:xs) /= n = error "lenght of the list must be equal to the first parameter"
+    | otherwise = factorial k `div` factProd 1 (x:xs)
+        where
+            k = sum' (x:xs)
+            factProd  y []     = y
+            factProd  y (x:xs) = factProd ( y * factorial x) xs
+
+stirling :: Integer -> Integer ->  Integer
+stirling n 1 = 1
+stirling n 2 = ((2 ^n)- 2) `div` 2
+stirling n k
+    | k > n      = 0
+    | k == n     = 1
+    | k == (n-1) = combination n 2
+    | otherwise  = stirling (n-1) (k-1) +  k * stirling (n-1) k
